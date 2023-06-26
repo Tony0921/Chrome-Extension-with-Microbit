d2168a62a88d139982c4d21eb2745b925faccf40
陳棨煜 <tony32059613@gmail.com>
2023-06-26 13:07:10 +0800

    1.加入disconnect按鈕及功能 2.加入自動傳送

--- content.js
+++ content.js
@@ -1,39 +1,72 @@
+let isConnect = false;
+
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
         if (message.type === "connect") {
             connect();
+        }
+        if (message.type === "disconnect") {
+            disconnect();
+        }
+        if (message.method === "getData") {
+            console.log(message.method);
+            if (isConnect) {
+                sendResponse({ data: 'CONNECTED' }); // 這裡填寫你希望回傳給popup的資料
+            }
+            else {
+                sendResponse({ data: 'DISSCONECT' }); // 這裡填寫你希望回傳給popup的資料
+            }
         }
     }
 );
 let port;
 const connectButton = document.createElement('button');
 // const dataContainer = document.createElement('div');
+var first_prompt = "目前環境中的光度(light)和溫度temperature)，光度範圍為0~255，溫度以攝氏度表示，環境中有日光燈、檯燈、冷氣機、電風扇、電暖爐可供使用，請依據環境參數提供生活建議，字數不超過 30 字，以上說明若清楚，請回答 我瞭解了";
 
-async function connect(){
+async function connect() {
     try {
         port = await navigator.serial.requestPort();
         await port.open({ baudRate: 115200 });
         reader = port.readable.getReader();
 
-        connectButton.disabled = true;
+        // connectButton.disabled = true;
+        isConnect = true;
+        setFieldValue(first_prompt);
+        clickSend();
+
         readLoop();
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-connectButton.addEventListener('click', async () => {
-    try {
-        port = await navigator.serial.requestPort();
-        await port.open({ baudRate: 115200 });
-        reader = port.readable.getReader();
+let disconnecting = false;
 
-        connectButton.disabled = true;
-        readLoop();
+async function disconnect() {
+    try {
+        if (port && !disconnecting) {
+            disconnecting = true;
+
+            if (reader) {
+                await reader.cancel();
+                reader.releaseLock();
+                reader = null;
+            }
+            if (port.readable) {
+                await port.close();
+                port = null;
+            }
+
+            disconnecting = false;
+        }
+        // connectButton.disabled = false;
+        isConnect = false;
     } catch (error) {
+        disconnecting = false;
         console.error('Error:', error);
     }
-});
+}
 
 async function readLoop() {
     try {
@@ -54,7 +87,8 @@ async function readLoop() {
     } finally {
         reader.releaseLock();
         port.close();
-        connectButton.disabled = false;
+        // connectButton.disabled = false;
+        isConnect = false;
     }
 }
 
@@ -68,20 +102,23 @@ function processInput(input) {
 
         if (value === 'l') {
             buffer = '';
-            buffer += value; 
+            buffer += value;
         } else if (value === '#') {
             // dataContainer.textContent = buffer;
             console.log(buffer);
             setFieldValue(buffer);
+            clickSend();
         } else {
-            buffer += value;  
+            buffer += value;
         }
     }
 }
 
 function setFieldValue(value) {
-    var inputField = document.getElementsByTagName("textarea")[0];
+    var inputField = document.getElementById("prompt-textarea");
     inputField.value = value;
+    inputField.dispatchEvent(new Event("input", { bubbles: true }));
+    inputField.focus();
 
     // set textfield height
     if (inputField.scrollHeight < 200) {
@@ -89,20 +126,16 @@ function setFieldValue(value) {
     } else {
         inputField.setAttribute("style", "max-height:200px; height:" + (inputField.scrollHeight) + "px;");
     }
+
 }
 
 function getSendBtn() {
-    var element_1 = document.querySelectorAll('button.absolute.p-1');
-    return element_1[0];
+    var element = document.querySelectorAll('button.absolute.p-1');
+    return element[0];
 }
 
-function checkFieldValue(value) {
+function clickSend() {
     var sendBtn = getSendBtn();
-
-    // enable button
-    if (value != "") {
-        sendBtn.disabled = false;
-    } else {
-        sendBtn.disabled = true;
-    }
+    sendBtn.click();
+    canSend = false;
 }
--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,39 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("connect-btn").addEventListener("click", function () {
         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
             var activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, {type: "connect"});
+            chrome.tabs.sendMessage(activeTab.id, { type: "connect" });
+        });
+    });
+    document.getElementById("disconnect-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { type: "disconnect" });
+            chrome.tabs.sendMessage(tabs[0].id, { method: "getData" }, function (response) {
+                if (response && response.data == "CONNECTED") {
+                    console.log(response.data);
+                    document.getElementById("connect-btn").disabled = true;
+                    document.getElementById("disconnect-btn").disabled = false;
+                }else if(response && response.data == "DISSCONECT"){
+                    console.log(response.data);
+                    document.getElementById("connect-btn").disabled = false;
+                    document.getElementById("disconnect-btn").disabled = true;
+                }
+            });
+        });
+        
+    });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { method: "getData" }, function (response) {
+            if (response && response.data == "CONNECTED") {
+                console.log(response.data);
+                document.getElementById("connect-btn").disabled = true;
+                document.getElementById("disconnect-btn").disabled = false;
+            }else if(response && response.data == "DISSCONECT"){
+                console.log(response.data);
+                document.getElementById("connect-btn").disabled = false;
+                document.getElementById("disconnect-btn").disabled = true;
+            }
         });
     });
 });
